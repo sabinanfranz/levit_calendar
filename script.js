47bd960ca02761a8ff554b5ad0bbf16f819bd75a
nandanfranz <60725105+nandanfranz@users.noreply.github.com>
2023-06-13 17:17:56 +0900

    PM 5:17(End)

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const currentDate = document.querySelector(".current-date"),
   daysTag = document.querySelector(".days"),
   prevNextIcon = document.querySelectorAll(".icons span");
 
-let date = new Date(),
+let date = new Date(), //현재 날짜 및 시간
   currYear = date.getFullYear(),
   currMonth = date.getMonth();
-
-console.log(date, currYear, currMonth);
 
 const months = [
   "January",
@@ -24,15 +22,35 @@ const months = [
 ];
 
 const renderCalendar = () => {
-  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+  //getday는 요일을 가져옴, 0부터 일요일, 전 달의 1일의 요일을 가져옴
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
   let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
+  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
   let liTag = "";
 
-  //for 문
-  for (let i = 1; i <= lastDateofMonth; i++) {
-    liTag += `<li>${i}</li>`;
+  //for 문, 이전 달
+  for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
   }
-  console.log(lastDateofMonth);
+
+  //for 문, 현재 달
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    // 정확히 오늘 날짜만 class를 active로 지정
+    let isToday =
+      i === date.getDate() &&
+      currMonth === new Date().getMonth() &&
+      currYear === new Date().getFullYear()
+        ? "active"
+        : "";
+    liTag += `<li class="${isToday}">${i}</li>`;
+  }
+
+  //for 문, 다음 달
+  for (let i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
+
   currentDate.innerText = months[currMonth] + " " + currYear;
   daysTag.innerHTML = liTag;
 };
@@ -43,6 +61,14 @@ prevNextIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
     // if 문
     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+    if (currMonth < 0 || currMonth > 11) {
+      date = new Date(currYear, currMonth);
+      currYear = date.getFullYear();
+      currMonth = date.getMonth();
+    } else {
+      date = new Date();
+    }
     //달력 업데이트를 계속 해주는 것.
     renderCalendar();
   });
