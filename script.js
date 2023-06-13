7a8f392903bb35b4a83aec80b447c24edb0b90c7
nandanfranz <60725105+nandanfranz@users.noreply.github.com>
2023-06-13 16:10:28 +0900

    PM 4:10

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const currentDate = document.querySelector(".current-date");
+const currentDate = document.querySelector(".current-date"),
+  daysTag = document.querySelector(".days");
+prevNextIcon = document.querySelectorAll(".icons span");
 
 let date = new Date(),
   currYear = date.getFullYear(),
@@ -6,8 +8,33 @@ let date = new Date(),
 
 console.log(date, currYear, currMonth);
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const renderCalendar = () => {
-  currentDate.innerText = currMonth + " " + currYear;
+  let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  let liTag = "";
+
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    liTag += `<li>${i}</li>`;
+  }
+  console.log(lastDateofMonth);
+  currentDate.innerText = months[currMonth] + " " + currYear;
+  daysTag.innerHTML = liTag;
 };
 
 renderCalendar();
+
+preNextIcon.forEach(icon = )
